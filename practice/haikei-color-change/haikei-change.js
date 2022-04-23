6ea85118fe4031768e406b82e0668fc3b0961347
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-04-24 08:54:40 +0900

    やっと背景色変えられるようになったよ

--- practice/haikei-color-change/haikei-change.js
+++ practice/haikei-color-change/haikei-change.js
@@ -1,4 +1,8 @@
+console.log('関数の外だからこれは実行されるはずだね');
+
 haikei_change = () => {
-  console.log('コンテントスクリプトだお');
+  console.log('関数内部のコンソールログ');
   document.body.style.backgroundColor = "blue";
 }
+
+haikei_change();
